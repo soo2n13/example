5f4951e746d4f54102a435f94c0b8e9da1b9402c
lee <soo2n13@naver.com>
2020-05-28 11:20:42 +0900

    .gitignore 파일 추가

---
+++ Step02_DataType/src/test/main/Lab1Class.java
@@ -0,0 +1,7 @@
+package test.main;
+
+public class Lab1Class {
+	public static void main(String[] args) {
+		//ÀÛ¾÷Áß...
+	}
+}
--- Step02_DataType/src/test/main/MainClass01.java
+++ Step02_DataType/src/test/main/MainClass01.java
@@ -1,4 +1,4 @@
-package test.main;
+ package test.main;
 
 public class MainClass01 {
 	public static void main(String[] args) {
